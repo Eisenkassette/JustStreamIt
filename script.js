6159ad435fe16ba03563356492abad90ba4920dd
Daniel <daniel.larionov@protonmail.com>
2024-05-21 23:05:02 +0200

    custom sections done

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ async function fetchMovies(score, genre = "") {
     return data.results;
 }
 
+async function fetchAllGenreNames(page = 1, allGenreNames = []) {
+    try {
+        const response = await fetch(`http://localhost:8000/api/v1/genres/?movie_title_contains=&name=+&name_contains=&page=${page}`);
+        const data = await response.json();
+
+        const currentGenreNames = data.results.map(genre => genre.name);
+        allGenreNames.push(...currentGenreNames);
+
+        // If there are more pages, recursively fetch them
+        if (data.next) {
+            return fetchAllGenreNames(page + 1, allGenreNames);
+        } else {
+            // No more pages, return all genre names
+            return allGenreNames;
+        }
+    } catch (error) {
+        console.error('Error fetching genre names:', error);
+        return []; // Return an empty array in case of an error
+    }
+}
+
 async function getTopMoviesByGenre(genre) {
     let movies = [];
     let score = 10;
@@ -98,13 +119,21 @@ getTopMoviesByGenre("drama").then(genreTwoMovies => {
 });
 
 
-const genreSelect = document.getElementById('genre_1_select');
-genreSelect.addEventListener('change', () => {
-    // Get the value of the selected option
-    const selectedGenre = genreSelect.value;
 
-    getTopMoviesByGenre(selectedGenre).then(custOneMovies => {
-    console.log(custOneMovies);
+const genreOneSelect = document.getElementById('genre_1_select');
+const genreTwoSelect = document.getElementById('genre_2_select');
+
+fetchAllGenreNames().then(genreNames => {
+    genreNames.forEach(genreName => {
+        genreOneSelect.innerHTML += `<option value="${genreName}">${genreName}</option>`;
+        genreTwoSelect.innerHTML += `<option value="${genreName}">${genreName}</option>`;
+    })
+}).catch(error => {
+    console.error('Error:', error);
+});
+
+genreOneSelect.addEventListener('change', () => {
+    getTopMoviesByGenre(genreOneSelect.value).then(custOneMovies => {
     for (let i = 0; i <= 6; i++) {
         const genreOneMovieBlock = document.getElementById(`custom_1_movie_block_${i}`);
         const genreOneMovieName = document.getElementById(`custom_1_movie_name_${i}`);
@@ -115,9 +144,25 @@ genreSelect.addEventListener('change', () => {
             console.log(`Could not find elements for index ${i}`);
         }
     }
-}).catch(error => {
-    console.error('Error fetching movies:', error);
+    }).catch(error => {
+        console.error('Error fetching movies:', error);
+    });
 });
 
+genreTwoSelect.addEventListener('change', () => {
+    getTopMoviesByGenre(genreTwoSelect.value).then(custTwoMovies => {
+    for (let i = 0; i <= 6; i++) {
+        const genreOneMovieBlock = document.getElementById(`custom_2_movie_block_${i}`);
+        const genreOneMovieName = document.getElementById(`custom_2_movie_name_${i}`);
+        if (genreOneMovieBlock && genreOneMovieName) {
+            genreOneMovieBlock.style.backgroundImage = `url('${custTwoMovies[i].image_url}')`;
+            genreOneMovieName.textContent = custTwoMovies[i].title;
+        } else {
+            console.log(`Could not find elements for index ${i}`);
+        }
+    }
+    }).catch(error => {
+        console.error('Error fetching movies:', error);
+    });
 });
 
